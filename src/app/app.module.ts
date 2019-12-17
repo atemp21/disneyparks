06c4dc4637cc40b66117004f6ec24eb445122046
atemp21 <aaron.templeton@live.com>
2019-12-17 11:46:04 -0700

    added angular material module, app routing module and app navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './angular-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AppNavbarComponent
+    ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularMaterialModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
